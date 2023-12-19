fecbbc5085ac7d41964b803d31bd0bbfa806f140
marian4cost <mariana.costa.lisboa5579@gmil.com>
2023-12-19 15:59:47 -0300

    palmeiras n tem mundial

--- monitoramentoNivelDagua.ino
+++ monitoramentoNivelDagua.ino
@@ -1,6 +1,5 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-#include <NewPing.h>
 
 const char *ssid = "17282";
 const char *password = "123456789";
@@ -9,7 +8,6 @@ const int triggerPin = 5;
 const int echoPin = 4;
 
 ESP8266WebServer server(80);
-NewPing sonar(triggerPin, echoPin);
 
 void setup() {
   Serial.begin(115200);
@@ -21,7 +19,7 @@ void loop() {
   server.handleClient();
 
   // Medir a distância e exibir no Monitor Serial
-  float distancia_cm = sonar.ping_cm();
+  float distancia_cm = sonarPing();
   Serial.print("Distância: ");
   Serial.print(distancia_cm);
   Serial.println(" cm");
@@ -38,12 +36,21 @@ void conectarWiFi() {
   Serial.println("Conectado ao WiFi");
 }
 
+float sonarPing() {
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  return pulseIn(echoPin, HIGH) * 0.034 / 2.0;
+}
+
 void configurarServidor() {
   server.on("/", HTTP_GET, []() {
     Serial.print("Endereço IP: ");
     Serial.println(WiFi.localIP());
 
-    float distancia_cm = sonar.ping_cm();
+    float distancia_cm = sonarPing();
 
     String mensagem = "Distância: " + String(distancia_cm) + " cm";
 
